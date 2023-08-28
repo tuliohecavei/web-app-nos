4c44f5f3fc019aba93995c5af9545d8b3e772019
Tulio Hecavei <tuliohecavei@gmail.com>
2023-08-28 11:35:22 -0300

    att

---
+++ script.js
@@ -0,0 +1,7 @@
+function encurtar() {
+    let url = document.getElementById("input-url").value;
+    if (!url) {
+        alert("É precisor inserir uma url para encurtar");
+        return;
+    }
+}
