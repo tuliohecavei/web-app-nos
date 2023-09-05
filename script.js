995c05ddb983b68f1a9976a0d40e1356f022b8b0
Tulio Hecavei <tuliohecavei@gmail.com>
2023-09-05 11:50:18 -0300

    d

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 'use strict'
 
 const botãoEncurtar = document.getElementById('button');
-document.getElementById('input-url')
+const input = document.getElementById('input-url');
 
-const encurtar = async() => {
-  let urlEncurtar = document.getElementById('input-url').value;
+const encurtar = () => {
+  let urlEncurtar = input.value;
+
   if (!urlEncurtar) {
       alert("É preciso inserir uma URL para encurtar");
       return;  
   }
-  let headers = {
-  "Content-Type": "application/json"
-  }
-  let result = await lib.url.temporary['@0.3.0'].create({
-    url: urlEncurtar // required
-  });
 
+    console.log(urlEncurtar)
+  
 }
 
 botãoEncurtar.addEventListener('click', encurtar)
+
